91f9de30424d70d6647d76df6b8870c01cca9330
CptGrave <lukastrevo@gmail.com>
2023-03-25 16:52:58 +0100

    change directories of img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
+import trollface from "../../src/trollface.png"
 export default function Header() {
   return(
     <header className="header">
-      <img className="header--logo" src="../images/trollface.png"></img>
+      <img className="header--logo" src={trollface}></img>
       <h2 className="header--title">Meme Generator</h2>
       <h3 className="header--project">React course project 3</h3>
     </header>
